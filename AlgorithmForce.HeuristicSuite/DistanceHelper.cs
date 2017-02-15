1fa4f9554652226a54ae55cae8088c762b806924
rvhuang <rvhuang@outlook.com>
2017-02-15 19:16:49 +0800

    Added DistanceHelper.cs

---
+++ AlgorithmForce.HeuristicSuite/DistanceHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmForce.HeuristicSuite
+{
+    public static class DistanceHelper
+    {
+        public static long GetManhattanDistance(long x1, long y1, long x2, long y2)
+        {
+            return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+        }
+    }
+}
