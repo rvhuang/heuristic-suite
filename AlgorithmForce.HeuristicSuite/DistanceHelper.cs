48cd23cfc0c415e8950f08624209cd6c71815cbe
rvhuang <rvhuang@outlook.com>
2017-02-15 21:15:55 +0800

    Add GetChebyshevDistance method

--- AlgorithmForce.HeuristicSuite/DistanceHelper.cs
+++ AlgorithmForce.HeuristicSuite/DistanceHelper.cs
@@ -8,5 +8,10 @@ namespace AlgorithmForce.HeuristicSuite
         {
             return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
         }
+
+        public static long GetChebyshevDistance(long x1, long y1, long x2, long y2)
+        {
+            return Math.Max(Math.Abs(x1 - x2), Math.Abs(y1 - y2));
+        }
     }
 }
