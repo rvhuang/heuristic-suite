e0e8d42c35867c1b2a255ec77c6527ee2d42e2ca
rvhuang <rvhuang@outlook.com>
2017-03-23 13:48:55 +0800

    Add new example

---
+++ AlgorithmForce.Example.CoinsFlipping/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmForce.Example.CoinsFlipping
+{
+    using HeuristicSuite;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This example demonstrates how the game AI solves the coins flipping puzzle.");
+            Console.WriteLine("Only a pair of adjacent coins is allowed to be flipped over at same time.");
+            Console.WriteLine("All ten coins that are head is the goal.");
+            Console.WriteLine();
+            Console.WriteLine("The puzzle is inspired by brilliant.org:");
+            Console.WriteLine("https://brilliant.org/practice/flipping-pairs/?chapter=introduction-to-joy");
+            Console.WriteLine();
+
+            // true  -> head
+            // false -> tail
+            var goal = new bool[10] { true, true, true, true, true, true, true, true, true, true, };
+            var init = new bool[10] { true, false, true, false, true, true, false, true, false, true };
+
+            var engine = new AStar<bool[], Step<bool[]>>();
+
+            engine.EqualityComparer = new SequenceEqualityComparer<bool>();
+            engine.NextStepsFactory = GetNextSteps;
+
+            var solution = engine.ExecuteWith(new Step<bool[]>(init), goal, new PuzzleStateComparer()).Reverse();
+
+            foreach (var step in solution.Enumerate())
+            {
+                var coins = step.Key;
+
+                Console.WriteLine("Step {0}: {1}", step.Depth, step.Depth == 0 ? "(Initial)" : string.Empty);
+                Console.WriteLine(string.Join(", ", coins.Select(coin => coin ? "Head" : "Tail")));
+            }
+            Console.ReadKey(true);
+        }
+
+        static IEnumerable<Step<bool[]>> GetNextSteps(Step<bool[]> coins)
+        {
+            for (var i = 0; i < 9; i++)
+            {
+                var copied = coins.Key.ToArray();
+
+                copied[i] = !copied[i];
+                copied[i+ 1] = !copied[i + 1];
+
+                yield return new Step<bool[]>(copied);
+            }
+        }
+
+        static void Print(bool[] array)
+        {
+            Console.WriteLine(string.Join(", ", array.Select(coin => coin ? "Head" : "Tail")));
+        }
+    }
+}
---
+++ AlgorithmForce.Example.CoinsFlipping/PuzzleStateComparer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+#if DEBUG
+using System.Diagnostics;
+#endif
+
+namespace AlgorithmForce.Example.CoinsFlipping
+{
+    class PuzzleStateComparer : Comparer<bool[]>
+    {
+        public static IReadOnlyList<bool> Goal = new bool[10] { true, true, true, true, true, true, true, true, true, true, };
+
+        public override int Compare(bool[] x, bool[] y)
+        {
+            if (x == null) return 1;
+            if (y == null) return -1;
+
+            var countXHeads = x.Count(coin => coin); // higher is better 
+            var countYHeads = y.Count(coin => coin);
+            var continuityX = GetContinuity(x); // higher is better
+            var continuityY = GetContinuity(y);
+
+#if DEBUG
+            Debug.WriteLine("X: {0}{1}", string.Join(", ", x.Select(coin => coin ? "Head" : "Tail")), string.Empty);
+            Debug.WriteLine("countXHeads: {0}, continuityX: {1}, total: {2}", countXHeads, continuityX, countXHeads + continuityX);
+            Debug.WriteLine("Y: {0}{1}", string.Join(", ", y.Select(coin => coin ? "Head" : "Tail")), string.Empty);
+            Debug.WriteLine("countYHeads: {0}, continuityY: {1}, total: {2}", countYHeads, continuityY, countYHeads + continuityY);
+#endif
+            return Comparer<int>.Default.Compare(countYHeads + continuityY, countXHeads + continuityX);
+        }
+
+        private static int GetContinuity(bool[] coins)
+        {
+            var finalScore = 0;
+            var currentScore = 0;
+
+            for (var i = 1; i < coins.Length; i++)
+            {
+                if (coins[i - 1] && coins[i])
+                    currentScore += 1;
+                else
+                {
+                    if (finalScore < currentScore)
+                        finalScore = currentScore;
+
+                    currentScore = 0;
+                }
+            }
+
+            if (finalScore < currentScore)
+                finalScore = currentScore;
+
+            return finalScore;
+        }
+    }
+}
